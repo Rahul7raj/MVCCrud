0f5c25f57625e2599c17b6b6a31713e68b3de6f3
Rahul7raj <69787135+Rahul7raj@users.noreply.github.com>
2022-12-24 17:33:34 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using MyThirdProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyThirdProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        practicedbEntities1 dbobj = new practicedbEntities1(); //created database object
+        // GET: Student
+        public ActionResult CreateStudent()  //for display view
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StoreStudent(tbl_student Model) // for storing data into db
+        {
+            if (ModelState.IsValid)
+            {
+                tbl_student tobj = new tbl_student(); //table object
+                tobj.FName = Model.FName;
+                tobj.LName = Model.LName;
+                tobj.Mobile_Number = Model.Mobile_Number;
+                tobj.Email = Model.Email;
+                tobj.About = Model.About;
+
+                dbobj.tbl_student.Add(tobj);
+                dbobj.SaveChanges();
+
+                return RedirectToAction("ReadData");
+            }
+            return View("CreateStudent");
+        }
+
+        [HttpGet]
+        public ActionResult ReadData() //displaying data
+        {
+            using(dbobj)
+            {
+                var data = dbobj.tbl_student.ToList();
+                return View(data);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyThirdProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyThirdProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b5b60a4-2613-412e-a891-1eedd91f5122")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
